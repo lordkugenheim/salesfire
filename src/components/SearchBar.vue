2a1be792f0be63e7b57e400b6bf6120781fd3a46
Ben <ben@Ben-taylor.co.uk>
2025-04-27 14:11:23 +0100

    add prototype API request

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,4 +8,38 @@
 
 <script>
 
+    async function getData() {
+
+        const url = "https://aix.salesfire.co.uk/api/searcha";
+
+        var params = {
+            client_id : 'dbf1dbc9-a940-48c2-b44b-0bb6dc63924e',
+            query : '',
+            limit : 4,
+            page : 1
+        };
+
+        const urlParams = [];
+
+        for (var key in params) {
+            urlParams.push(key + '=' + encodeURIComponent(params[key]));
+        }
+
+        try {
+            const response = await fetch(url + '?' + urlParams.join('&'));
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const json = await response.json();
+            console.log(json);
+        } catch (error) {
+            console.error(error.message);
+        }
+
+    }
+
+    getData();
+
 </script>
